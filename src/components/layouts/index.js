04f99aa4944eaa968d966d24fe87ac8641b52a9d
Imagine Chiu <chihfan.chiu@igroup.tw>
2018-03-19 09:28:25 +0800

    新增 next/scss設定

--- src/components/layouts/index.js
+++ src/components/layouts/index.js
@@ -5,11 +5,23 @@ import '@styles/app.scss';
 
 export default class Layout extends React.PureComponent {
     static propTypes = {
+        title: PropTypes.string,
         children: PropTypes.string.isRequired
     };
+
+    static defaultProps = {
+        title: ''
+    };
+
     render() {
         return (
             <div className="layout-base">
+                {this.props.title && (
+                    <Head>
+                        <title>{this.props.title}</title>
+                    </Head>
+                )}
+
                 {this.props.children}
             </div>
         );
