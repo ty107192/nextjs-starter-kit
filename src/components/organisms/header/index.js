44bce7a406f25d69c64c09b479c91980047999d8
Imagine Chiu <chihfan.chiu@igroup.tw>
2018-03-21 20:26:06 +0800

    更改 響應式排版

--- src/components/organisms/header/index.js
+++ src/components/organisms/header/index.js
@@ -13,21 +13,17 @@ export default class Header extends React.Component {
             <HeaderCustom>
                 <div className="container">
                     <div className="row">
-                        <div className="col-6">
-                            <div className="row">
-                                <div className="col-auto">
-                                    <Title>
-                                        <a href="javascript:;" onClick={() => Router.pushRoute('home')}>
-                                            Nextjs Starter Kit
-                                        </a>
-                                    </Title>
-                                </div>
-                                <div className="col">
-                                    <NavMenu/>
-                                </div>
-                            </div>
+                        <div className="col-12 col-sm-auto">
+                            <Title>
+                                <a href="javascript:;" onClick={() => Router.pushRoute('home')}>
+                                    Nextjs Starter Kit
+                                </a>
+                            </Title>
                         </div>
-                        <div className="col-6 text-right">
+                        <div className="col-5">
+                            <NavMenu/>
+                        </div>
+                        <div className="col text-right">
                             <NavLang/>
                         </div>
                     </div>
