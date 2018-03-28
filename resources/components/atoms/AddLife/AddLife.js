ff5c291a39d9e6be492bcf9d02aa7b7e9c6850a1
Imagine Chiu <chihfan.chiu@igroup.tw>
2018-03-28 16:17:10 +0800

    新增 storybook i18n問題測試

--- resources/components/atoms/AddLife/AddLife.js
+++ resources/components/atoms/AddLife/AddLife.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import withI18Next from '@hoc/withI18Next';
 
+@withI18Next
 
 export default class AddLife extends React.PureComponent {
     static propTypes = {
@@ -21,14 +23,16 @@ export default class AddLife extends React.PureComponent {
     }
 
     render() {
-        const {gameLife} = this.props;
+        const {gameLife, t} = this.props;
         return (
             <div className="row" style={{marginTop: '20px'}}>
                 <div className="col-auto" style={{fontSize: '20px'}}>
                     <span role="img" aria-label="life">💖 x{gameLife}</span>
                 </div>
                 <div className="col">
-                    <button className="btn btn-success btn-xs" onClick={this.add}>投幣</button>
+                    <button className="btn btn-success btn-xs" onClick={this.add}>
+                        {t('game.coin')}
+                    </button>
                     <span/>
                     <button className="btn btn-danger btn-xs" onClick={this.sub}>退幣</button>
                 </div>
