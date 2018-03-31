e73660be2a2f0974679bfa9ed7b3ee5b09791dc0
Imagine Chiu <chihfan.chiu@igroup.tw>
2018-03-31 13:57:30 +0800

    新增 Storybook 在靜態資源路由

--- test/storybook/middleware.js
+++ test/storybook/middleware.js
@@ -2,6 +2,16 @@ const express = require('express');
 const path = require('path');
 
 module.exports = function (router) {
+    // 開放靜態資源路由
+    router.use(
+        '/static',
+        express.static(path.join(
+            __dirname,
+            '../../static'
+        ))
+    );
+
+    // 開放i18next語系字典檔案路由
     router.use(
         '/lang',
         express.static(path.join(
