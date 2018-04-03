2eabf0dfa5db46f951ada6f05cf281ff715506e2
Imagine Chiu <chihfan.chiu@igroup.tw>
2018-04-03 10:30:55 +0800

    新增 i18next測試

--- test/jest/__test__/index.test.js
+++ test/jest/__test__/index.test.js
@@ -6,6 +6,15 @@ import renderer from 'react-test-renderer';
 
 import AddLife from '../../../src/resources/components/atoms/AddLife/AddLife';
 
+//mock i18next
+jest.mock('react-i18next', () => ({
+    // this mock makes sure any components using the translate HoC receive the t function as a prop
+    translate: () => Component => props => <Component t={(code) => code} {...props} />,
+}));
+
+
+
+
 describe('With AddLife', () => {
 
 
@@ -34,15 +43,24 @@ describe('With AddLife', () => {
         // wrapper.find('button').eq(0).simulate('click');
         wrapper.simulate('click');
         // expect(wrapper.prop('gameLife')).toBe(1);
-        expect(wrapper.find('button').length).toBe(1);
+        // expect(wrapper.find('button').length).toBe(1);
+        expect(wrapper.find('button').text()).toBe('1234');
 
-        console.log(wrapper.debug());
+
+        // console.log(wrapper.debug());
 
 
-    })
-
+    });
     it('子元素必須存在', () => {
         const wrapper = shallow(<AddLife addLife={addLife} subLife={subLife} gameLife={gameLife}/>);
-        expect(wrapper.children().length).toBe(1);
-    })
+        expect(wrapper.children().length).toBe(0);
+    });
+    it('測試i18n', () => {
+        const wrapper = mount(
+                <AddLife addLife={addLife} subLife={subLife} gameLife={gameLife}/>
+        );
+        // console.log(wrapper.debug());
+
+        // expect(wrapper.children().length).toBe(1);
+    });
 })
--- test/jest/config.js
+++ test/jest/config.js
@@ -1,10 +1,5 @@
 const path = require('path');
 
-// module.exports = {
-//     rootDir: ".",
-//     setupFiles: ['<rootDir>/setup.js'],
-//     testPathIgnorePatterns: [path.resolve(__dirname, '../../.next/'), path.resolve(__dirname, '../../node_modules/')]
-// };
 const appRoot = path.resolve(__dirname);
 module.exports = require('@talentui/jest-config')({
     roots: [appRoot],
