5243b4a03c8c2f0ebfabc774b54a4ca320d06a9d
Imagine <imagine10255@gmail.com>
2018-04-17 23:22:35 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withSass = require('@zeit/next-sass');
 
 module.exports = withSass({
     assetPrefix: process.env.PROXY_URL ? process.env.PROXY_URL : '', //PROXY_URL路徑
-    useFileSystemPublicRoutes: false,//使用文件系統路由
+    useFileSystemPublicRoutes: false,//是否使用文件系統路由
     exportPathMap: webRouter,
 
     webpack: function (config) {
